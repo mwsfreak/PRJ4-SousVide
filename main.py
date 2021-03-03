6579ff659f000a4484f13a591b710debec4ea9a0
mwsfreak <m.franks@live.dk>
2021-03-03 14:42:24 +0100

    Testing with .kv file

--- main.py
+++ main.py
@@ -1,33 +1,21 @@
 import kivy
 kivy.require('1.0.6') # replace with your current kivy version !
 
-
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
-class LoginScreen(GridLayout):
+class HomePage(Widget):
+    pass
 
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-
-        self.cols = 2
-
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-
-        self.add_widget(Label(text='Password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
-
-
-class MyApp(App):
+class SousVideApp(App):
 
     def build(self):
-        return LoginScreen()
+        return HomePage()
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    SousVideApp().run()
